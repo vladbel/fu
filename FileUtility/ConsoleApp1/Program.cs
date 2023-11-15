2213ef17b9baa1ae93934bf2efe7c57c86e2a76f
Vladimir Beliaev <vbeliaev@revspringinc.com>
2023-11-15 16:29:46 -0600

    init

---
+++ FileUtility/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+﻿using System;
+using fu.lib;
+
+namespace fu.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = args[0];
+            var count = int.Parse(args[1]);
+
+            var threads = new List<Thread>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var fileName = Path.Combine(directory, $"test{i}.txt");
+                var content = $"content{i}";
+
+                var thread = new Thread(() => {
+                    var util = new CreateAndAccessUtility();
+                    util.Run(fileName, content);
+                });
+                threads.Add(thread);
+            }
+
+            threads.ForEach(thread => thread.Start());
+            threads.ForEach(thread => thread.Join());
+
+        }
+    }
+}
---
+++ FileUtility/fu.lib/CreateAndAccessUtility.cs
@@ -0,0 +1,45 @@
+﻿namespace fu.lib
+{
+    public class CreateAndAccessUtility
+    {
+        public void Run(string fileName, string content)
+        {
+
+            var thread1 = new Thread(() => {
+                var cf = new CreateFile();
+                cf.Run(fileName, content);
+            });
+
+            var thread2 = new Thread(() => {
+                var af = new AccessFile();
+                af.Run(fileName);
+            });
+
+            thread1.Start();
+            thread1.Join();
+
+            thread2.Start();
+            thread2.Join();
+        }
+    }
+}
+
+public class CreateFile
+{
+    public void Run ( string fileName, string content) 
+    {
+        Console.WriteLine($"create file: {fileName}");
+        //File.Create(fileName);
+        File.WriteAllText(fileName, content);
+    }
+}
+
+public class AccessFile
+{
+    public void Run ( string fileName) 
+    {
+        Console.WriteLine($"Reading file: {fileName}");
+        var content = File.ReadAllText(fileName);
+        Console.WriteLine(content);
+    }
+}
